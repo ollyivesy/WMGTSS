c3807be40a4d37e087edd3fa5e518b3c91c966c9
ollyivesy <ives.uk@gmail.com>
2022-02-04 23:35:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,25 @@ function load() {
     /* Calculate how many days there are in a month*/
     const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-    console.log(daysInMonth);
+    /* Get the first day of the month*/
+    const firstDayOfMonth = new Date(year, month, 1);
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-gb', {
+
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+
+    });
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    for(let i = 1; i<= paddingDays + daysInMonth; i++) {
+
+        const daySquare = document.createElement('div');
+    }
+
+    console.log(paddingDays);
 }
 
 load();
