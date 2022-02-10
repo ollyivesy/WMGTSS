3394a2489b1d3b8a1f5cab27ecbfecb649116201
ollyivesy <ives.uk@gmail.com>
2022-02-10 23:17:56 +0000

    Login Page addition

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
+
+//state to use the necassary files for the index.ejs are in the "public" folder
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+    
+})
 
 app.listen(3000)
