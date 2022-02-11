794a673158b3d30586a14a817f9fa5489c9fb056
ollyivesy <ives.uk@gmail.com>
2022-02-11 17:34:28 +0000

    Student View creation

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
 
 const initializePassport = require('./passport-config')
 initializePassport(passport, username => {
     users.find(user => user.username === username)
+    id => users.find(user => user.id === id)
 })
 
 const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 //state to use the necassary files for the index.ejs are in the "public" folder
 app.use(express.static(__dirname + '/public'))
@@ -24,9 +40,11 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
-    
-})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 
 
